8ff37ca8365dd656286ae11cc54aa520d7b20998
Rutvi Suthar <rutvi.suthar@example.com>
2025-08-13 13:11:05 +0530

    Initial dashboard commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css"; // Tailwind CSS
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/my-react-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/krisha-crm-dashboard/', // 👈 GitHub repo name with slashes
   plugins: [react()],
-});
+  base: '/my-react-website/', // your GitHub repo name
+})
